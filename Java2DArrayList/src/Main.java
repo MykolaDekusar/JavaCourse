4988d9a3ebfc5a59cb36d981a6ccd39ba8f0c481
MykolaDekusar <ndekusar97@gmail.com>
2024-10-29 17:07:00 +0100

    Fixed small ArrayList issues

--- Java2DArrayList/src/Main.java
+++ Java2DArrayList/src/Main.java
@@ -6,16 +6,16 @@ public class Main {
 		//Un arrayList 2d è un insieme di arrayList
 		//molto simile ad un array 2d
 		//dobbiamo importare java.util.*
-		ArrayList<Integer> voti = new ArrayList();
+		ArrayList<Integer> voti = new ArrayList<Integer>();
 		voti.add(5);
 		voti.add(3);
 		voti.add(8);
-		ArrayList<Integer> data = new ArrayList();
+		ArrayList<Integer> data = new ArrayList<Integer>();
 		data.add(12);
 		data.add(21);
 		data.add(2);
 		//inizializziamo l'arrayList 2d
-		ArrayList<ArrayList<Integer>> nomiData = new ArrayList();
+		ArrayList<ArrayList<Integer>> nomiData = new ArrayList<ArrayList<Integer>>();
 		//Aggiungiamo all'arrayList 2d i nostri ArrayList singoli
 		nomiData.add(voti);
 		nomiData.add(data);
