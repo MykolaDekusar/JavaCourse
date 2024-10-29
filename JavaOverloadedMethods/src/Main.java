74c2cf10ff5cd0a63f31af58e44c6f78e4abc609
MykolaDekusar <ndekusar97@gmail.com>
2024-10-29 17:52:50 +0100

    Overloaded method somma to get a sum of 2 and 3 variables

--- JavaOverloadedMethods/src/Main.java
+++ JavaOverloadedMethods/src/Main.java
@@ -2,8 +2,22 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Ovreloaded methods = sono metodi che condividono lo stesso nome ma hanno
+		//                     parametri diversi
+		//nome metodo + parametri = firma del metodo
+		int a = 10;
+		int b=5;
+		int c=2;
+		somma(a,b);
+		somma(a,b,c);
+ 	}
+	//primo metodo di sovraccarico
+	static int somma(int x, int y) {
+		return x+y;
+	}
+	//secondo metodo di sovraccarico
+	static int somma(int x, int y, int z) {
+		return x+y+z;
 	}
 
 }
