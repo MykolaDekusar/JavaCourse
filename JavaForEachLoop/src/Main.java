7be618ab62e2e31605597501a4c8a54e4d449fe5
MykolaDekusar <ndekusar97@gmail.com>
2024-10-29 15:47:09 +0100

    For-each loop test with strings

--- JavaForEachLoop/src/Main.java
+++ JavaForEachLoop/src/Main.java
@@ -2,7 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//for-each loop è meno flessibile però è piu comodo
+		//funziona sia per gli array che le collection (ArrayList)
+		String[] animali = {"cane","gatto"};
+		//per ogni stringa indice in animali stampami l'animale
+		for(String i : animali) System.out.println(i);
 	}
 
 }
