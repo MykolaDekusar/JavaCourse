0ec3e46900890d186559a4788ccef6e184f5ff01
MykolaDekusar <ndekusar97@gmail.com>
2024-10-29 16:25:30 +0100

    Created static void method to print name and age called from main method. Use of arguments and params

--- JavaMethods/src/Main.java
+++ JavaMethods/src/Main.java
@@ -2,8 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Metodo = blocco di codice che viene eseguito quando il metodo viene richiamato
+		//tra le parentesi possiamo passarle delle variabili
+		String nome="Nico";
+		int eta = 18;
+		hello(nome,eta);
+	}
+	//solitamente static non dobbiamo metterlo ma in questo caso siccome
+	//lo stiamo usando dentro static void main allora dobbiamo metterlo anche per hello
+	//il tipo void non ha nessun return cioè non restituisce dati
+	static void hello(String nome, int eta) {
+		System.out.println("Hello!!! "+nome);
+		System.out.println("Hai "+eta+" anni");
 	}
 
 }
