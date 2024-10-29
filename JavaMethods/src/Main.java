c600b53668c8318d5201d33e3a92916aae7ca694
MykolaDekusar <ndekusar97@gmail.com>
2024-10-29 16:40:47 +0100

    Creation of a simple method that adds 2 integers and returns the sum

--- JavaMethods/src/Main.java
+++ JavaMethods/src/Main.java
@@ -7,6 +7,9 @@ public class Main {
 		String nome="Nico";
 		int eta = 18;
 		hello(nome,eta);
+		int x=20;
+		int y=12;
+		System.out.println(calcolo(x,y));
 	}
 	//solitamente static non dobbiamo metterlo ma in questo caso siccome
 	//lo stiamo usando dentro static void main allora dobbiamo metterlo anche per hello
@@ -14,6 +17,10 @@ public class Main {
 	static void hello(String nome, int eta) {
 		System.out.println("Hello!!! "+nome);
 		System.out.println("Hai "+eta+" anni");
+	}
+	//creiamo un metodo che ci restituisce la somma di 2 numeri interi
+	static int calcolo(int a,int b) {
+		return a+b;
 	}
 
 }
