f740cc8afa7aa2fc22c6fbdc1d0c1a9b7bf9541a
MykolaDekusar <ndekusar97@gmail.com>
2024-10-29 18:07:14 +0100

    test of overloaded methods changing primitive values of variables

--- JavaOverloadedMethods/src/Main.java
+++ JavaOverloadedMethods/src/Main.java
@@ -8,8 +8,14 @@ public class Main {
 		int a = 10;
 		int b=5;
 		int c=2;
-		somma(a,b);
-		somma(a,b,c);
+		System.out.println(somma(a,b));
+		System.out.println(somma(a,b,c));
+		//cambiamo il valore primitivo
+		double x=2.342;
+		double z=3.14;
+		double l=23.123;
+		System.out.println(somma(x,l));
+		System.out.println(somma(l,z,x));
  	}
 	//primo metodo di sovraccarico
 	static int somma(int x, int y) {
@@ -17,6 +23,14 @@ public class Main {
 	}
 	//secondo metodo di sovraccarico
 	static int somma(int x, int y, int z) {
+		return x+y+z;
+	}
+	//cambiando il tipo di dato primitivo da int a double possiamo creare altri metodi
+	static double somma (double x, double y) {
+		return x+y;
+	}
+	//
+	static double somma(double x, double y, double z) {
 		return x+y+z;
 	}
 
