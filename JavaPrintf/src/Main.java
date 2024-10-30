e519a9bed5d7cd5bc7d85e96c26bdab6bbe8b324
MykolaDekusar <ndekusar97@gmail.com>
2024-10-30 16:45:38 +0100

    Definition of printf()

--- JavaPrintf/src/Main.java
+++ JavaPrintf/src/Main.java
@@ -2,6 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
+		//printf() = un metodo opzionale per formattare, stampare e formattare i dati in console
+		//           richiede 2 argomenti= una stringa + (oggetto/variabile/valore)
+		//           % seguito da [flags] [precisione] [larghezza] [carattere di conversione]
 		
 	}
 
