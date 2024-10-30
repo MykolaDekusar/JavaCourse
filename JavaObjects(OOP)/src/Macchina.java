d44b377c192c4c52ff439359c64f634ae9a3e76c
MykolaDekusar <ndekusar97@gmail.com>
2024-10-30 18:06:31 +0100

    Creation of new Car object containing attributes and methods

---
+++ JavaObjects(OOP)/src/Macchina.java
@@ -0,0 +1,20 @@
+
+public class Macchina {
+	//definiamo gli attributi
+	String marca = "Hyundai";
+	String modello = "i20N";
+	String colore = "Bianco";
+	int velocitaMax = 240;
+	float consumi=6.7f;
+	
+	//definiamo i metodi
+	
+	void accellera() {
+		System.out.println("Accelleraaaa");
+	}
+	
+	void frena() {
+		System.out.println("Freeeeeeeeeeeeena");
+	}
+	
+}
--- JavaObjects(OOP)/src/Main.java
+++ JavaObjects(OOP)/src/Main.java
@@ -6,7 +6,25 @@ public class Main {
 		//gli attributi sono gli aspetti di un oggetto
 		//i metodi sono le cose che può fare un oggetto
 		//esempi: telefono, tavolo, computer, mouse
+		//esempti attributo telefono: colore, peso, dimensioni
+		//esempi metodi telefono: chiama(), gioca(), riproduci()
 		
+		//CREIAMO UN OGGETTO MACCHINA AL DI FUORI DELL'OGGETO Main
+		
+		//Ora creiamone un istanza
+		Macchina macchina = new Macchina();
+		//Per accedere agli attributi e ai metodi del nuovo oggetto usiamo il punto .
+		System.out.println(macchina.colore);
+		System.out.println(macchina.consumi);
+		System.out.println(macchina.marca);
+		System.out.println(macchina.modello);
+		//Richiamiamo i metodi
+		macchina.accellera();
+		macchina.frena();
+		
+		//Possiamo creare piu oggetti Macchina ma avranno tutte le stesse caratteristiche
+		//Nella prossima lezione vediamo i COSTRUTTORI che ci permettono di creare oggetti
+		//con caratteristiche diverse
 	}
 
 }
