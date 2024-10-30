cc0fdd35009a65381d040be722af78847f995c6e
MykolaDekusar <ndekusar97@gmail.com>
2024-10-30 17:03:31 +0100

    Definition and use of final for variables

---
+++ JavaFinalKeyword/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//final = l'uso di final BLOCCA il valore di una variabile e non sarà piu
+		//possibile riassegnarle il valore
+		final double PI = 3.13159;
+		//per buona norma una variabile final si scrive in MAIUSCOLO
+		System.out.println("Sono una variabile final "+PI);
+	}
+
+}
