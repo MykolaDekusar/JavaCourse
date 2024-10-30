56015f7d376d7ab4546d857cb204bf6a6cf5c0ee
MykolaDekusar <ndekusar97@gmail.com>
2024-10-30 17:57:55 +0100

    Object definition

---
+++ JavaObjects(OOP)/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//object = un istanza di una classe che può contenere attributi e metodi
+		//gli attributi sono gli aspetti di un oggetto
+		//i metodi sono le cose che può fare un oggetto
+		//esempi: telefono, tavolo, computer, mouse
+		
+	}
+
+}
