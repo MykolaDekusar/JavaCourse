887dfd7ec202ee64495c00ceccf272fbdb428b90
MykolaDekusar <ndekusar97@gmail.com>
2024-10-31 12:58:36 +0100

    Sending params in object creation and printing each object variables data

--- JavaConstructor/src/Main.java
+++ JavaConstructor/src/Main.java
@@ -6,6 +6,10 @@ public class Main {
 		Macchina macchina1 = new Macchina("Hyundai", "i20N", 240, 6.7f);
 		//creiamo una nuova macchina
 		Macchina macchina2 = new Macchina("Mercedes", "CLS", 280, 8.0f);
+		
+		//stampiamo i valori di entrambi gli oggetti
+		System.out.println(macchina1.marca+" "+ macchina1.modello+" "+macchina1.vMax+" "+macchina1.consumi);
+		System.out.println(macchina2.marca+" "+ macchina2.modello+" "+macchina2.vMax+" "+macchina2.consumi);
 	}
 
 }
