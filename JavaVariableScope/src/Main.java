07a669452cc4e089a01ad334a8d19fb7ffa18fac
MykolaDekusar <ndekusar97@gmail.com>
2024-10-31 15:45:33 +0100

    Init

---
+++ JavaVariableScope/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// local = dichiarato dentro il metodo, visibile solo al metodo stesso
+		// global = dichiarato fuori dal metodo ma dentro la classe, visibile a tutte
+		// le parti della classe 
+
+	}
+
+}
