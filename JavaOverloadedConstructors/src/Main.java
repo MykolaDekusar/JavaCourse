029ac8e4322aad48154ec20f15355952e0880d6d
MykolaDekusar <ndekusar97@gmail.com>
2024-10-31 16:47:10 +0100

    Creation of multiple overloaded constructor methods that get different number of params and printing of each pizza in console

--- JavaOverloadedConstructors/src/Main.java
+++ JavaOverloadedConstructors/src/Main.java
@@ -2,8 +2,44 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Sono simili ai overloaded methods
+		//Overloaded Constructors = più costruttori dentro una classe che hanno lo stesso nome
+		//                          ma il numero di parametri diverso
+		//                          nome + parametri = firma
+		
+		//ABBIAMO CREATO 4 COSTRUTTORI CON LO STESSO NOME MA PARAMETRI DIVERSI
+		//creiamo una pizza che manda 4 argomenti
+		Pizza pizzaCon4Argomenti = new Pizza("Integrale","Pomodoro","Mozzarella","Salame");
+		System.out.println(pizzaCon4Argomenti.impasto);
+		System.out.println(pizzaCon4Argomenti.salsa);
+		System.out.println(pizzaCon4Argomenti.formaggio);
+		System.out.println(pizzaCon4Argomenti.topping);
+		
+		System.out.println();
+		//creiamo una pizza che manda 3 argomenti
+		Pizza pizzaCon3Argomenti = new Pizza("Normale","Spinaci","Gorgonzola");
+		System.out.println(pizzaCon3Argomenti.impasto);
+		System.out.println(pizzaCon3Argomenti.salsa);
+		System.out.println(pizzaCon3Argomenti.formaggio);
+			
+		System.out.println();
+		//creiamo una pizza che manda 2 argomenti
+		Pizza pizzaCon2Argomenti = new Pizza("Carbone","Latte");
+		System.out.println(pizzaCon2Argomenti.impasto);
+		System.out.println(pizzaCon2Argomenti.salsa);
+		
+		System.out.println();
+		//creiamo una pizza che manda 1 argomento
+		Pizza pizzaCon1Argomenti = new Pizza("Insetti");
+		System.out.println(pizzaCon1Argomenti.impasto);
+		
+		System.out.println();
+		//creiamo una pizza senza argomenti
+		Pizza pizzaSenzaArgomenti = new Pizza();
+		System.out.println(pizzaSenzaArgomenti.impasto);
+		System.out.println(pizzaSenzaArgomenti.salsa);
+		System.out.println(pizzaSenzaArgomenti.formaggio);
+		System.out.println(pizzaSenzaArgomenti.topping);
 	}
 
 }
--- JavaOverloadedConstructors/src/Pizza.java
+++ JavaOverloadedConstructors/src/Pizza.java
@@ -1,4 +1,40 @@
 
 public class Pizza {
 
+	//inizializziamo delle variabili per creare la nostra pizza
+	String impasto;
+	String salsa;
+	String formaggio;
+	String topping;
+	//creiamo dei costruttori
+	
+	//COSTRUTTORE CHE HA 4 PARAMETRI
+	Pizza(String impasto, String salsa, String formaggio, String topping){
+		this.impasto=impasto;
+		this.salsa=salsa;
+		this.formaggio=formaggio;
+		this.topping=topping;
+	}
+	
+	//COSTRUTTORE CHE HA 3 PARAMETRI
+	Pizza(String impasto, String salsa, String formaggio){
+		this.impasto=impasto;
+		this.salsa=salsa;
+		this.formaggio=formaggio;
+	}
+	
+	//COSTRUTTORE CHA HA 2 PARAMETRI
+	Pizza(String impasto, String salsa){
+		this.impasto=impasto;
+		this.salsa=salsa;
+	}
+	
+	//COSTRUTTORE HA 1 PARAMETRO
+	Pizza(String impasto){
+		this.impasto=impasto;
+	}
+	
+	//COSTRUTTORE SENZA PARAMETRI
+	Pizza(){
+	}
 }
