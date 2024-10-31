544ac1c3ab726edfaddf9354a139e7b108a62d54
MykolaDekusar <ndekusar97@gmail.com>
2024-10-31 17:21:01 +0100

    Implicit and explicit explanation of toString()

--- JavatoStringMethod/src/Main.java
+++ JavatoStringMethod/src/Main.java
@@ -2,8 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//toString() = metodo speciale che tutti gli oggetti ereditano e che restituscie
+		//             una stringa che "testualmente rappresenta" un oggetto
+		//             puo essere inplicita o esplicita
+		Macchina car = new Macchina();
+		//il print e println contengono implicitamente il toString()
+		System.out.println(car);
+		//oppure possiamo richiamarlo in modo esplicito
+		System.out.println(car.toString());
+		//l'abbiamo sovrascritto nell'oggetto Macchina per restituirci una stringa con i dati dell'oggetto
 	}
 
 }
