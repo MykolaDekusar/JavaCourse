668280915e27ce54871573d5e0fe4a2061f964a0
MykolaDekusar <ndekusar97@gmail.com>
2024-10-31 12:48:19 +0100

    Initialization of variables for the constructor method, creation of constructor, use of this. to associate variables to the current object instance and creation of 2 car objects

--- JavaConstructor/src/Macchina.java
+++ JavaConstructor/src/Macchina.java
@@ -2,4 +2,19 @@
 public class Macchina {
 
 	//inizializziamo le variabili necessarie per il costruttore
+	String marca;
+	String modello;
+	int vMax;
+	float consumi;
+	
+	//scriviamo il costruttore con gli argomenti che prende
+	Macchina(String marca, String modello, int vMax, float consumi){
+		//this serve ad indicare l'istanza corrente dell'oggetto es:
+		//nel caso di macchina1 this sarà associato a macchina1
+		//nel caso di macchina2 this sarà associato a macchina2 etc..
+		this.marca=marca;
+		this.modello=modello;
+		this.vMax=vMax;
+		this.consumi=consumi;
+	}
 }
--- JavaConstructor/src/Main.java
+++ JavaConstructor/src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		//creiamo un nuovo oggetto macchina1 e macchina2 associandole valori diversi
-		
+		Macchina macchina1 = new Macchina("Hyundai", "i20N", 240, 6.7f);
+		//creiamo una nuova macchina
+		Macchina macchina2 = new Macchina("Mercedes", "CLS", 280, 8.0f);
 	}
 
 }
