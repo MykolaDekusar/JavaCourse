135451ecb0dfeb14281e9f4ff1c6509638c82697
MykolaDekusar <ndekusar97@gmail.com>
2024-11-02 17:05:52 +0100

    Created a small interactive app to ask the user to insert a number of foods in the fridge, looping on the number we ask the user to input a name, we create a new object and we insert it in the fridge array. After that using the for-each loop we print all the names in the console

--- JavaArrayOfObjects/src/Main.java
+++ JavaArrayOfObjects/src/Main.java
@@ -4,8 +4,27 @@ public class Main {
 	public static void main(String[] args) {
 		//Chiediamo all'utente quanti cibi vuole inserire
 		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("Inserisci quanti cibi vuoi aggiungere al frigo: ");
-		System.out.println("Inserisci il nome di un cibo");
+		int quantita = scanner.nextInt();
+		scanner.nextLine();
+		
+		//imposto la lunghezza dell'array di oggetti cibo all'input dell'utente
+		Cibo[] frigo = new Cibo[quantita];
+		//Chiedo all'utente di inserire nomne del cibo e creo una classe e la inserisco
+		//nell'array frigo
+		for(int i=1;i<=quantita;i++) {
+			System.out.println("Inserisci il nome del cibo n°"+i);
+			String ciboInserito = scanner.nextLine();
+			Cibo cibo  = new Cibo(ciboInserito);
+			frigo[i-1] = cibo;
+		}
+		scanner.close();
+		//Ora stampiamo i nomi dei cibi inseriti dall'utente
+		for(Cibo i : frigo) {
+			System.out.println(i.nome);
+		}
+		
 	}
 
 }
