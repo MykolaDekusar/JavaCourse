e6d58d911a5c3ce35291eb90ab8f181a348d96fc
MykolaDekusar <ndekusar97@gmail.com>
2024-11-02 16:44:28 +0100

    Creation of Food object class

---
+++ JavaArrayOfObjects/src/Cibo.java
@@ -0,0 +1,8 @@
+
+public class Cibo {
+
+	String nome;
+	Cibo(String nomeCibo){
+		this.nome = nomeCibo;
+	}
+}
--- JavaArrayOfObjects/src/Main.java
+++ JavaArrayOfObjects/src/Main.java
@@ -1,8 +1,11 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//Chiediamo all'utente quanti cibi vuole inserire
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Inserisci quanti cibi vuoi aggiungere al frigo: ");
+		System.out.println("Inserisci il nome di un cibo");
 	}
 
 }
