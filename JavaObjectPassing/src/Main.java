89ffa8036e27ba98e154105678fa21e05c0b449e
MykolaDekusar <ndekusar97@gmail.com>
2024-11-03 16:09:05 +0100

    Created car objects and garage object, passed car object to the garage park method

--- JavaObjectPassing/src/Main.java
+++ JavaObjectPassing/src/Main.java
@@ -2,7 +2,16 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//creiamo delle macchine
+		Macchina auto1 = new Macchina("BMW");
+		Macchina auto2 = new Macchina("Mercedes");
+		Macchina auto3 = new Macchina("Audi");
+		//creiamo l'oggetto garage
+		Garage garage = new Garage();
+		//applichiamo il metodo parcheggia passondo gli oggetti auto
+		garage.parcheggia(auto1);
+		garage.parcheggia(auto2);
+		garage.parcheggia(auto3);
 	}
 
 }
