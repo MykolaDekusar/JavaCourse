c14d2486b5ad49d43b3fd60c5ba644157aba9750
MykolaDekusar <ndekusar97@gmail.com>
2024-11-03 16:36:38 +0100

    Created static counter of number of frineds in Friend class

--- JavaStaticKeyword/src/Amico.java
+++ JavaStaticKeyword/src/Amico.java
@@ -1,3 +1,12 @@
-
 public class Amico {
+	String nome;
+	//creiamo un contatore statico per la classe di Amico
+	static int nAmici;
+	
+	Amico(String nome){
+		this.nome = nome;
+		//incrementiamo il contatore statico globale di Amico ogni volta che aggiungo un amico
+		nAmici++;
+	}
+	
 }
--- JavaStaticKeyword/src/Main.java
+++ JavaStaticKeyword/src/Main.java
@@ -4,6 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 		//static = un modificatore. Assegnando static a una variabile/metodo rende quella
 		//         variabile/metodo globale alla classe
+		
+		Amico amico1 = new Amico("Marco");
+		Amico amico2 = new Amico("Luca");
+		Amico amico3 = new Amico("Sara");
+		
+		System.out.println(amico1.nome);
+		System.out.println(amico2.nome);
+		System.out.println(amico3.nome);
 	}
 
 }
