f708850cc25d240d5a4a90402bce288c67260340
MykolaDekusar <ndekusar97@gmail.com>
2024-11-03 16:06:59 +0100

    Created Car constructro and Garage method to display parked car

--- JavaObjectPassing/src/Garage.java
+++ JavaObjectPassing/src/Garage.java
@@ -1,4 +1,7 @@
 
 public class Garage {
-
+	//creiamo un metodo che prende l'oggetto auto e stampa che è parcheggiata
+	void parcheggia(Macchina auto) {
+		System.out.println("L'auto "+auto.marca+" è parcheggiata");
+	}
 }
--- JavaObjectPassing/src/Macchina.java
+++ JavaObjectPassing/src/Macchina.java
@@ -1,4 +1,8 @@
 
 public class Macchina {
-
+	String marca;
+	//creiamo un oggetto macchina con la marca
+	Macchina(String marca){
+		this.marca = marca;
+	}
 }
