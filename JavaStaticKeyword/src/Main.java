8e5fba5df5c785690e8be522dfa5435c61f3e871
MykolaDekusar <ndekusar97@gmail.com>
2024-11-03 16:32:56 +0100

    Init

---
+++ JavaStaticKeyword/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//static = un modificatore. Assegnando static a una variabile/metodo rende quella
+		//         variabile/metodo globale alla classe
+	}
+
+}
