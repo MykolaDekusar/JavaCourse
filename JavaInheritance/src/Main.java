ea8d0ac4524d44c9e23bbd48dd534a82e47c7fd5
MykolaDekusar <ndekusar97@gmail.com>
2024-11-04 15:04:03 +0100

    Init

---
+++ JavaInheritance/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//inheritance ( eredità) = il processo dove una classe acquisisce gli attributi
+		//                         e i metodi di un altra classe
+	}
+
+}
