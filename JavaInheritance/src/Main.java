d1dc0452b5fc2e728cc041dd84c8cbb2ccd2dd80
MykolaDekusar <ndekusar97@gmail.com>
2024-11-04 15:09:24 +0100

    Use of Veicolo methods in auto and moto objects using inheritance

--- JavaInheritance/src/Main.java
+++ JavaInheritance/src/Main.java
@@ -4,6 +4,17 @@ public class Main {
 	public static void main(String[] args) {
 		//inheritance ( eredità) = il processo dove una classe acquisisce gli attributi
 		//                         e i metodi di un altra classe
+		
+		Auto auto = new Auto();
+		Moto moto = new Moto();
+		//siccome auto e moto ereditano Veicolo abbiamo accesso ai suoi metodi
+		auto.vai();
+		moto.vai();
+		auto.stop();
+		moto.stop();
+		//ma anche ai metodi e attributi delle singole classi auto e moto
+		System.out.println(auto.ruote);
+		System.out.println(moto.ruote);
 	}
 
 }
