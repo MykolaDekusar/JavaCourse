a46deed9172b152e338e64a8e86e72efa31c2f01
MykolaDekusar <ndekusar97@gmail.com>
2024-11-18 20:12:46 +0100

    Tested if the constructor worked

--- JavaEncapsulation/src/Main.java
+++ JavaEncapsulation/src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Auto auto = new Auto();
+		Auto auto = new Auto("Hyundai", "i20N", 2021);
+		System.out.println(auto.getMarca());
+		System.out.println(auto.getModello());
 		System.out.println(auto.getAnno());
 	}
 
