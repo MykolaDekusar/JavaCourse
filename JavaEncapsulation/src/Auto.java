3b1dcfbc72712761d45ea1409cc9c712af056a0d
MykolaDekusar <ndekusar97@gmail.com>
2024-11-18 20:06:30 +0100

    Created getters for Auto

--- JavaEncapsulation/src/Auto.java
+++ JavaEncapsulation/src/Auto.java
@@ -1,4 +1,17 @@
 
 public class Auto {
-
+	//creiamo delle variabili private
+	private String marca;
+	private String modello;
+	private int anno;
+	//creiamo dei getter e dei setter
+	public String getMarca() {
+		return marca;
+	}
+	public String getModello() {
+		return modello;
+	}
+	public int getAnno() {
+		return anno;
+	}
 }
