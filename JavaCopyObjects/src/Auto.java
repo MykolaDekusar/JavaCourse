f51121dfdd774f6256b64b9849c8dba2f853f63c
MykolaDekusar <ndekusar97@gmail.com>
2024-11-20 13:51:03 +0100

    Created class Auto with getters, setters and constructor

--- JavaCopyObjects/src/Auto.java
+++ JavaCopyObjects/src/Auto.java
@@ -1,4 +1,34 @@
 
 public class Auto {
-
+	private String maker;
+	private String model;
+	private int year;
+	//creo il constructor
+	Auto(String maker, String model, int year){
+		this.setMaker(maker);
+		this.setModel(model);
+		this.setYear(year);
+	}
+	//creo i setter
+	public void setMaker(String maker) {
+		this.maker=maker;
+	}
+	
+	public void setModel(String model) {
+		this.model=model;
+	}
+	
+	public void setYear(int year) {
+		this.year=year;
+	}
+	//creo i getter
+	public String getMaker() {
+		return this.maker;
+	}
+	public String getModel() {
+		return this.model;
+	}
+	public int getYear() {
+		return this.year;
+	}
 }
--- JavaCopyObjects/src/Main.java
+++ JavaCopyObjects/src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Auto auto1 = new Auto("Chevrolet", "Camaro", 1995);
+		System.out.println(auto1.getMaker());
 	}
 
 }
